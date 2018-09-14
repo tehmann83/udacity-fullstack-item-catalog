32731f36b82b197d365bb06fcbfca2225860b617
Thomas Ehmann <ehmannth@gmail.com>
2018-09-14 12:43:02 +0200

    Changed style of code

--- vagrant/catalog/views.py
+++ vagrant/catalog/views.py
@@ -139,8 +139,10 @@ def gconnect():
 
 
 def createUser(login_session):
-    newUser = User(name=login_session['username'], email=login_session[
-                   'email'], picture=login_session['picture'])
+    newUser = User(
+        name=login_session['username'],
+        email=login_session['email'],
+        picture=login_session['picture'])
     session.add(newUser)
     session.commit()
     user = session.query(User).filter_by(email=login_session['email']).one()
