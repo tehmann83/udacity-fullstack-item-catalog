918db6f35bda3780634ec4e90c99b53f98a50933
Thomas Ehmann <ehmannth@gmail.com>
2018-09-14 23:49:36 +0200

    Changed size of user image

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
 
 Base = declarative_base()
 
@@ -15,6 +16,13 @@ class User(Base):
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
     picture = Column(String(250))
+    # password_hash = Column(String(64))
+    #
+    # def hash_password(self, password):
+    #     self.password_hash = pwd_context.encrypt(password)
+    #
+    # def verify_password(self, password):
+    #     return pwd_context.verify(password, self.password_hash)
 
 
 class Category(Base):
